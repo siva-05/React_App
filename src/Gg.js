d8a2024e3f509ed5d21f5c3188e55a21c8bd860f
Siva <122322784+siva-05@users.noreply.github.com>
2023-02-20 12:33:54 +0530

    Add files via upload

---
+++ src/Gg.js
@@ -0,0 +1,13 @@
+
+
+function Gg(){
+    return(
+
+        
+<div class="titles">
+    
+      <img src="https://see.fontimg.com/api/renderfont4/xgMV/eyJyIjoiZnMiLCJoIjo1NCwidyI6MTI1MCwiZnMiOjQzLCJmZ2MiOiIjRjRFRkVGIiwiYmdjIjoiIzg4N0ZBRSIsInQiOjF9/RGVudmVyIFN0b3Jlcw/bulgatti.png" />
+</div>
+    );
+}
+export default Gg;
---
+++ src/Products.js
@@ -0,0 +1,118 @@
+function Products(){
+    return(
+        <div class="Products">
+
+
+        
+
+
+        <div class="box">
+            <a href="payment.html">
+            <div class="box-1">
+                <img src="https://www.redwolf.in/image/cache/catalog/mens-t-shirts/iron-man-arc-reactor-glow-in-the-dark-t-shirt-438x438.gif" alt="nil" />
+            </div>
+            <div class="box-2">
+                <p>Arc Reactor Marvel Official T-shirt</p>
+                <p>T-shirt</p>
+                <p>Rs. 450</p>
+            </div>
+            </a>
+        </div>
+
+        <div class="box">
+            <a href="payment.html">
+            <div class="box-1">
+                <img src="https://www.redwolf.in/image/cache/catalog/sweatshirts/warrior-thor-hoodie-india-438x438.jpg" alt="nil" />
+            </div>
+            <div class="box-2">
+                <p>Warrior Thor - Marvel Official Hoodie</p>
+                <p>Hoodie</p>
+                <p>Rs. 450</p>
+            </div>
+            </a>
+        </div>
+
+        <div class="box">
+            <a href="payment.html">
+            <div class="box-1">
+                <img src="https://www.redwolf.in/image/cache/catalog/sweatshirts/DTG/spider-man-dark-logo-hoodie-india-438x438.jpg" alt="nil" />
+            </div>
+            <div class="box-2">
+                <p>Spider-Man: Dark Logo -Hoodie</p>
+                <p>Hoodie</p>
+                <p>Rs. 768</p>
+            </div>
+            </a>
+        </div>
+
+        <div class="box">
+            <a href="payment.html">
+            <div class="box-1">
+                <img src="https://www.redwolf.in/image/cache/catalog/mens-t-shirts/Digital/come-on-what-did-you-expect-t-shirt-india-438x438.jpg" alt="nil" />
+            </div>
+            <div class="box-2">
+                <p>Loki: Come On - Marvel Official T-shirt</p>
+                <p>T-shirt</p>
+                <p>Rs. 879</p>
+            </div>
+            </a>
+        </div>
+
+        <div class="break"></div>
+
+        <div class="box">
+            <div class="box-1">
+                <img src="https://www.redwolf.in/image/cache/catalog/sweatshirts/avengers-character-logos-hoodie-india-438x438.jpeg" alt="nil" />
+            </div>
+            <div class="box-2">
+                <p>Avengers: Character Logos - Marvel Official Hoodie</p>
+                <p>Hoodie</p>
+                <p>Rs. 879</p>
+            </div>
+        </div>
+
+        
+        <div class="box">
+            <div class="box-1">
+                <img src="https://www.redwolf.in/image/cache/catalog/mens-t-shirts/Digital/come-on-what-did-you-expect-t-shirt-india-438x438.jpg" alt="nil" />
+            </div>
+            <div class="box-2">
+                <p>Loki: Come On - Marvel Official T-shirt</p>
+                <p>T-shirt</p>
+                <p>Rs. 879</p>
+            </div>
+        </div>
+
+        
+        <div class="box">
+            <div class="box-1">
+                <img src="https://www.redwolf.in/image/cache/catalog/mens-t-shirts/Digital/black-widow-art-t-shirt-india-438x438.jpg " alt="nil" />
+            </div>
+            <div class="box-2">
+                <p>Loki: Come On - Marvel Official T-shirt</p>
+                <p>T-shirt</p>
+                <p>Rs. 879</p>
+            </div>
+        </div>
+
+        
+        <div class="box">
+            <div class="box-1">
+                <img src="https://www.redwolf.in/image/cache/catalog/mens-t-shirts/drop%20cut/black-panther-suit-drop-cut-t-shirt-india-438x438.jpg" alt="nil" />
+            </div>
+            <div class="box-2">
+                <p>title="Black Panther Supersuit - Marvel Official Drop Cut T-shirt"</p>
+                <p>T-shirt</p>
+                <p>Rs. 879</p>
+            </div>
+        </div>
+
+        
+        <div class="break"></div>
+
+        
+    </div>
+    );
+}
+
+export default Products;
